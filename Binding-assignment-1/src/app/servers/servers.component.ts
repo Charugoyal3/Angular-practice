74bbe1e189817095a395504271e1892a13667bbc
charugoyal3 <charugoyal180@gmail.com>
2021-03-11 13:54:32 +0530

    added

---
+++ Binding-assignment-1/src/app/servers/servers.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  username='TTN';
+  name="Charu";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onInputServe(event:Event){
+    this.username=(<HTMLInputElement>event.target).value;
+  }
+}
