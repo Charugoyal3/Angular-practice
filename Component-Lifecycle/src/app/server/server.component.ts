619434feee6c0d17b71c8316cf8b3b8566d6b8b9
charugoyal3 <charugoyal180@gmail.com>
2021-04-06 17:04:39 +0530

    added project

--- Component-Lifecycle/src/app/server/server.component.ts
+++ Component-Lifecycle/src/app/server/server.component.ts
@@ -28,34 +28,34 @@ OnDestroy
   @Input() data: string;
   lifecycleTicks: number = 0;
   constructor() { 
-    console.log("Constructor Called");
+    console.log(" This is called first -> Constructor Called");
   }
   ngOnChanges(changes: SimpleChanges){
-    console.log("ngOnChanges Called");
+    console.log("This is called after constructor -> ngOnChanges Called");
     console.log(changes);
     this.lifecycleTicks++;
 
   }
 
   ngOnInit(){
-    console.log("ngOnInit is Called!");
+    console.log(" After OnChanges -> ngOnInit is Called!");
   }
 
   ngDoCheck(){
-    console.log("Do Check Called!!");
+    console.log("After On Changes and Oninit -> Do Check Called!!");
   }
   ngAfterContentInit(){
-    console.log("NgAfterContentInit Called!!");
+    console.log("It is called after first doCheck -> NgAfterContentInit Called!!");
   }
   ngAfterContentChecked(){
-    console.log("NgAfterContentChecked called!!");
+    console.log("It gets called after ngAfterContentInit and every subsequent ngDoCheck-> NgAfterContentChecked called!!");
   }
 
   ngAfterViewInit(){
-    console.log("NgAfterViewInit Called!!");
+    console.log("It responds after a component's view, or a child component's view is initialized-> NgAfterViewInit Called!!");
   }
   ngAfterViewChecked(){
-    console.log("NgAfterViewChecked called!!");
+    console.log("It gets called after ngAfterViewInit, and it responds after the component's view, or the child component's view is checked -> NgAfterViewChecked called!!");
   }
   ngOnDestroy(){
     console.log("Destroy Called");
